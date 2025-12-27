9ebefb4a623b8e5eb23fcbe4e1bfe5486de0ea2d
OluwatomisinDev <oluwatomisindan@gmail.com>
2025-12-27 15:43:42 +0100

    ghuj

---
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
+  "static/chunks/node_modules_d1d89a41._.js",
+  "static/chunks/components_ThemeProvider_tsx_9c4da763._.js"
+],
+    source: "dynamic"
+});
